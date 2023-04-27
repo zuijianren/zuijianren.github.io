f5040bf2bc35f63db5643d7fb3c5ca8dd3f118f1
夏纤云 <yiqiejiaogeizaixia@gmail.com>
2023-04-27 22:11:54 +0800

    test

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs/",
+  base: "/",
   title: "Blog",
   description: "zuijianren's blog",
   themeConfig: {
